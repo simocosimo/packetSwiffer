7614ce767788c3eabc8bb4a5d1398df3fa5045d8
Simone Cosimo <simosx4@gmail.com>
2022-10-18 00:07:18 +0200

    Now using clap to parse arguments and use them

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use timer;
 use chrono;
 
-use std::{env, thread};
+use std::thread;
 use std::process;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::channel;
@@ -12,19 +12,18 @@ use std::io::Write;
 
 use pcap::{Device, Capture};
 use packet_swiffer::parser::handle_ethernet_frame;
+use packet_swiffer::args::Args;
+
+use clap::Parser;
 
 fn main() {
 
-    let interface_name = match env::args().nth(1) {
-        Some(n) => n,
-        None => {
-            eprintln!("USAGE: swiffer <NETWORK INTERFACE>");
-            process::exit(1);
-        }
-    };
-
-    let promisc_mode = env::args().nth(2) == Some("--promisc".to_string());
+    let args = Args::parse();
+    let interface_name = args.interface;
+    let promisc_mode = args.promisc;
     println!("Promisc mode: {}", promisc_mode);
+    let report_delay = args.timeout;
+    let report_fm = args.filename;
 
     // Find the network interface with the provided name
     let interfaces = Device::list().unwrap();
@@ -83,9 +82,10 @@ fn main() {
         loop {
             let mut buffer = Vec::<String>::new();
             let timer_flag_clone = timer_flag.clone();
-            let pathname = format!("report-{}.txt", index);
+            // TODO: maybe add timestamp to report filename? Or folder is better?
+            let pathname = format!("{}-{}.txt", report_fm, index);
             let path = Path::new(&pathname);
-            let _guard = timer.schedule_with_delay(chrono::Duration::seconds(10), move | | {
+            let _guard = timer.schedule_with_delay(chrono::Duration::seconds(report_delay), move | | {
                 let mut flag = timer_flag_clone.lock().unwrap();
                 *flag = true;
             });
