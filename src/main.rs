bad91b9ae9c6f274a0f3948154aed0b3806bea67
Damiano Ferla <ferla.damiano@gmail.com>
2022-10-25 10:43:55 +0200

    fix: report will be generated properly
    TBT more accurately

--- src/main.rs
+++ src/main.rs
@@ -124,37 +124,25 @@ fn main() {
 
     // TODO: create thread that analyze packets and produce report (synch should be done with mutex on structure)
     let report_thread = thread::spawn(move | | {
-        //let timer = timer::Timer::new();
+        let timer = timer::Timer::new();
+        let timer_flag_clone = timer_flag.clone();
         let mut index = 0;
-        loop {
+        let _guard_timer = timer.schedule_repeating(chrono::Duration::seconds(10), move || {
+            // Prendi pause lock
+            // Controlla se pause == true
+            // Se si, drop(guard_timer)
             let (lock, cvar) = &*pair3;
-            let mut buffer = Vec::<String>::new();
-            let timer_flag_clone = timer_flag.clone();
-            let pathname = format!("report-{}.txt", index);
-            let path = Path::new(&pathname);
-            let timer_thread = thread::spawn(move || {
-                let mut time = 10;
-                while time > 0 {
-                    std::thread::sleep(std::time::Duration::from_secs(1));
-                    let mut pause_flag = lock.lock().unwrap();
-                    if *pause_flag == false {
-                        let mut flag = timer_flag_clone.lock().unwrap();
-                        *flag = true;
-                        time = time - 1;
-                    }
-                    drop(pause_flag);
-                }
-            });
-            /*
-            let _guard_timer = timer.schedule_with_delay(chrono::Duration::seconds(10), move || {
-                // Prendi pause lock
-                // Controlla se pause == true
-                // Se si, drop(guard_timer)
+            let mut pause_flag = lock.lock().unwrap();
+            if *pause_flag == false{
                 let mut flag = timer_flag_clone.lock().unwrap();
                 *flag = true;
-            });
-            */
-
+            }
+            drop(pause_flag);
+        });
+        loop {
+            let mut buffer = Vec::<String>::new();
+            let pathname = format!("report-{}.txt", index);
+            let path = Path::new(&pathname);
             while let Ok(packet) = rx_report.recv() {
                 // TODO: here we should aggregate info about the traffic in a smart way
                 let tmp_string = String::from(format!("REPORT: {}", packet));
@@ -180,7 +168,6 @@ fn main() {
             }
             println!("[{}] Report #{} generated", chrono::offset::Local::now(), index);
             index += 1;
-            timer_thread.join().unwrap();
         }
     });
 
